f5beccc010de2ab2eedc461bb9c1d00716b7d366
muirujackson <jakezweru@gmail.com>
2024-05-30 22:20:59 +0300

    added cart

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Variation, ReviewRating, ProductGallery
+import admin_thumbnails
 
-# Register your models here.
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock', 'category', 'created_at', 'updated_at')
     list_filter = ('category', 'created_at')
     list_editable = ('price', 'stock', 'category')
     prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductGalleryInline]
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
--- store/views.py
+++ store/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from category.models import Category
-# from carts.models import CartItem
+from carts.models import CartItem
 from django.db.models import Q
 
-# from carts.views import _cart_id
+from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse
 # from .forms import ReviewForm
@@ -40,30 +40,30 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        #in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
-    if request.user.is_authenticated:
-        try:
-            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
-        except OrderProduct.DoesNotExist:
-            orderproduct = None
-    else:
-        orderproduct = None
+    # if request.user.is_authenticated:
+    #     try:
+    #         orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    #     except OrderProduct.DoesNotExist:
+    #         orderproduct = None
+    # else:
+    #     orderproduct = None
 
-    # Get the reviews
-    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+    # # Get the reviews
+    # reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
-    # Get the product gallery
-    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+    # # Get the product gallery
+    # product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
 
     context = {
         'single_product': single_product,
-        'in_cart'       : in_cart,
-        'orderproduct': orderproduct,
-        'reviews': reviews,
-        'product_gallery': product_gallery,
+        #'in_cart'       : in_cart,
+        #'orderproduct': orderproduct,
+        #'reviews': reviews,
+        #'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
